bfaf775e335f52027dc5e28aa06e36364bdf8f8e
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-05-31 02:26:03 -0400

    Bypassed plugin prompts

--- apps/react-shaper/src/index.ts
+++ apps/react-shaper/src/index.ts
@@ -1,23 +1,14 @@
-import inquirer from 'inquirer';
-// @ts-ignore
-import inquirerDirectory from 'inquirer-directory';
 import yargs from 'yargs/yargs';
 import { reactShaper } from './react-shaper';
-
-// Register inquirer prompts
-inquirer.registerPrompt('directory', inquirerDirectory);
 
 async function main() {
   // Parse command line
   const argv = await yargs().parse(process.argv.slice(2));
   const { _, $0: command, ...options } = argv; // eslint-disable-line
 
-  // Set pluginId if specified on the command line
-  // Otherwise, leave it as undefined
-  let pluginId;
-  if (_.length > 0 && typeof _[0] === 'string') {
-    pluginId = _[0];
-  }
+  // Specify pluginId and generator
+  const pluginId = '@code-shaper/react-starter';
+  options['generator'] = 'app';
 
   // Run reactShaper
   await reactShaper.run(pluginId, options);
